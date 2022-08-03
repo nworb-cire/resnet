5f2d0c58ba46bfc69d7958c85cd964d13670a930
Eric Brown <ebrown1@snapfinance.com>
2022-08-03 09:30:18 -0600

    fastmath

--- lazyjacobian.jl
+++ lazyjacobian.jl
@@ -15,7 +15,7 @@ Base.size(J::LazyJac) = (length(J.bi), length(J.bi)*length(J.x))
 function Base.:*(A::LinearAlgebra.Adjoint{T, Vector{T}}, J::LazyJac{T}) where T
     out = spzeros(length(J.x), length(J.bi))
     I = (first(p) for p in pairs(J.bi) if last(p))
-    @inbounds for i = I
+    @inbounds @fastmath for i = I
         out[:,i] .= A[i]*J.x
     end
     return out'
