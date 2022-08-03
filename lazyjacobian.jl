c26569813a190c746fc20931f8d5ca9fc93e6b1f
Eric Brown <ebrown1@snapfinance.com>
2022-08-03 09:12:58 -0600

    VJP computation is sparse

--- lazyjacobian.jl
+++ lazyjacobian.jl
@@ -1,4 +1,5 @@
 import LinearAlgebra
+using SparseArrays
 
 struct LazyJac{T<:Real}# <: AbstractMatrix
     x::Vector{T}
@@ -12,18 +13,18 @@ Base.size(J::LazyJac) = (length(J.bi), length(J.bi)*length(J.x))
 # end
 
 function Base.:*(A::LinearAlgebra.Adjoint{T, Vector{T}}, J::LazyJac{T}) where T
-    z = zero(J.x)
-    out = Matrix{T}(undef, (length(J.bi), length(J.x)))
-    @inbounds for i = 1:length(J.bi)
-        out[i,:] .= ifelse(J.bi[i], A[i]*J.x, z)
+    out = spzeros(length(J.x), length(J.bi))
+    @inbounds for i = findall(J.bi)
+        out[:,i] .= A[i]*J.x
     end
-    return out
+    return out'
 end
 
 function Base.:*(A::LinearAlgebra.Adjoint{T, Matrix{T}}, J::LazyJac{T}) where T
+    @warn "Computing MJPs instead of VJPs uses a lot of memory and is not optimized!"
     z = zero(J.x)
     out = Array{T}(undef, size(A, 1), length(J.bi), length(J.x))
-    for i = 1:length(J.bi)
+    @inbounds for i = 1:length(J.bi)
         out[i,:] = ifelse(b, dot(A[i,:], J.x), z)
     end
     return out
