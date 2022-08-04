9a4202efc60c3209184fbe7755afa8c5f37e79d1
Eric Brown <ebrown1@snapfinance.com>
2022-08-03 19:50:27 -0600

    Add ResidualLayer

--- grads.jl
+++ grads.jl
@@ -1,5 +1,7 @@
 include("lazyjacobian.jl")
 using DataStructures: Deque
+using BlockDiagonals
+using LinearAlgebra: Diagonal, I
 
 # Weight jacobians
 function ∂∂W(ξ::Vector{T}, bi::BitVector) where T
@@ -8,7 +10,7 @@ function ∂∂W(ξ::Vector{T}, bi::BitVector) where T
     @warn "This operation is very slow and memory intensive! Please use lazy jacobians."
 	return BlockDiagonal(bi .* X) |> Array
 end
-function ∂∂W(l::Layer{T}, x::Vector{T}) where T
+function ∂∂W(l::AbstractNetworkLayer{T}, x::Vector{T}) where T
 	nout = length(l.b)
 	X = Iterators.repeated(x', nout) |> collect
 	bi = _∂∂b(l,x)
@@ -18,7 +20,7 @@ function ∂∂W(l::Layer{T}, x::Vector{T}) where T
 end
 
 # Bias jacobians
-function _∂∂b(l::Layer{T}, x::Vector{T}) where T
+function _∂∂b(l::AbstractNetworkLayer{T}, x::Vector{T}) where T
 	if l.activation
 		bi = muladd(l.W',x,l.b) .≥ 0
 	else
@@ -26,7 +28,7 @@ function _∂∂b(l::Layer{T}, x::Vector{T}) where T
 	end
 	return bi
 end
-function ∂∂b(l::Layer{T}, x::Vector{T}) where T
+function ∂∂b(l::AbstractNetworkLayer{T}, x::Vector{T}) where T
 	return Vector{Float32}(_∂∂b(l,x))
 end
 ∂∂b(ξ::Vector{T}, bi::BitVector) where T = Vector{T}(bi)
@@ -36,8 +38,14 @@ function ∂∂ξ(l::Layer{T}, x::Vector{T}) where T
 	bi = _∂∂b(l,x)
 	return bi .* l.W'
 end
+function ∂∂ξ(l::ResidualLayer{T}, x::Vector{T}) where T
+	bi = _∂∂b(l,x)
+	return l.eta*I(length(x)) .+ (bi .* l.W')
+end
 ∂∂ξ(l::Layer{T}, bi::BitVector) where T = ∂∂ξ(l.W, bi)
 ∂∂ξ(W::Matrix{T}, bi::BitVector) where T = bi .* W'
+∂∂ξ(l::ResidualLayer{T}, bi::BitVector) where T = ∂∂ξ(l.W, l.eta, bi)
+∂∂ξ(W::Matrix{T}, η::T, bi::BitVector) where T = η*I(length(bi)) .+ bi .* W'
 
 # Cost jacobian
 #  TODO: Allow specification of loss function
--- layers.jl
+++ layers.jl
@@ -1,31 +1,36 @@
-using BlockDiagonals
-using LinearAlgebra: Diagonal
 using NNlib: relu
-
-
-struct Layer{T<:Real}
-	W::Matrix{T}
-	b::Vector{T}
-	activation::Bool
-end
 
 function initialize(din,dout)
 	scale = √(6/(din+dout)) |> Float32  # Glorot uniform dist
 	A = scale * (2rand(Float32,din,dout).-1)
 end
 
+abstract type AbstractNetworkLayer{T<:Real} end
+struct Layer{T<:Real} <: AbstractNetworkLayer{T}
+	W::Matrix{T}
+	b::Vector{T}
+	activation::Bool
+end
 Layer((din,dout)::Pair, activation = true) = Layer(initialize(din,dout), zeros(Float32, dout), activation)
-
 function (l::Layer{T})(x::Vector{T}) where T
 	y = muladd(l.W', x, l.b)
-	if l.activation
-		return relu.(y)
-	else
-		return y
-	end
+	ifelse(l.activation, relu.(y), y)
 end
 
-function (ls::Vector{Layer{T}})(x::Vector{T}) where T
+struct ResidualLayer{T<:Real} <: AbstractNetworkLayer{T}
+	W::Matrix{T}
+	b::Vector{T}
+	eta::T
+	activation::Bool
+end
+ResidualLayer((din,dout)::Pair, eta = 1f0, activation = true) = ResidualLayer(initialize(din,dout), zeros(Float32, dout), eta, activation)
+function (l::ResidualLayer{T})(x::Vector{T}) where T
+	y = muladd(l.W', x, l.b)
+	y = ifelse(l.activation, relu.(y), y)
+	@. y + l.eta * x
+end
+
+function (ls::Vector{<:AbstractNetworkLayer{T}})(x::Vector{T}) where T
 	for l in ls
 		x = l(x)
 	end
